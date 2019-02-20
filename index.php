0ba35e7b85966d2e98da2ce169cc9b62146fded2
Jorge Cuases <zappy2310@gmail.com>
2019-02-20 05:04:09 -0500

    nav

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ and open the template in the editor.
   <body>
     <?php
       if(!empty($_SESSION['session'])){
-        include '../html/navbar-l.php';
+        include './view/html/navbar-l.php';
       }else{
-        include '../html/navbar.php';
+        include './view/html/navbar.php';
       }
      ?>
 
