68bf1b4fa0d08711ce78536e52b69d6f5b3ad12d
Jorge Cuases <zappy2310@gmail.com>
2019-02-20 05:19:33 -0500

    h

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php include './controller/con_session.php'; ?>
+<?php include '../../controller/con_session.php'; ?>
 <html>
     <head>
       <?php include './view/html/head.php'; ?>
